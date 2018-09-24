145d5be8311d04d903f1f3f20877f99e5f6e80c3
tpipernob <tiagopiperno@gmail.com>
2018-09-24 10:13:33 -0300

    Personalizando o Admin

--- Blog/nossoBlog/admin.py
+++ Blog/nossoBlog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Postagem)
+admin.AdminSite.site_header = "Administração do Nosso Blog"
+admin.AdminSite.site_title = "Nosso Blog"
+
+class PostagemAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao')
+    list_filter = ('titulo',)
+    search_fields = ('titulo', 'descricao', 'conteudo',)
+    list_editable = ('descricao',)
+
+admin.site.register(Postagem, PostagemAdmin)
--- Blog/nossoBlog/models.py
+++ Blog/nossoBlog/models.py
@@ -4,3 +4,9 @@ class Postagem(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.CharField(max_length=200)
     conteudo = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Postagens"
+
+    def __str__(self):
+        return self.titulo
